0ded2c7c92c8f2e0ecfb18db18b4fe4feb5f9c8f
Marina Sheigets <marinasejgec34@gmail.com>
2023-01-28 12:13:59 +0200

    feat: left sidebar ui

--- src/components/__molecules__/LeftSideBar/LeftSideBar.tsx
+++ src/components/__molecules__/LeftSideBar/LeftSideBar.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import styled from 'styled-components';
+import theme from '../../../constants/globalStyles';
 
+const LINKS = [
+	{
+		id: 1,
+		name: 'Create Article',
+	},
+	{
+		id: 2,
+		name: 'Add Student',
+	},
+	{
+		id: 3,
+		name: 'Add Teacher',
+	},
+	{
+		id: 4,
+		name: 'Add Group',
+	},
+	{
+		id: 5,
+		name: 'Add Faculty',
+	},
+	{
+		id: 6,
+		name: 'Add Subject',
+	},
+];
 function LeftSideBar() {
-	return <div>LeftSideBar</div>;
+	return (
+		<Wrapper>
+			<UL>
+				{LINKS.map((item) => (
+					<li key={item.id}>{item.name}</li>
+				))}
+			</UL>
+		</Wrapper>
+	);
 }
+
+const Wrapper = styled('div')`
+	width: 250px;
+	padding: 2rem 0.5rem;
+	background: ${theme.background.blue.primary};
+	opacity: 57%;
+	height: 85vh;
+	z-index: 1;
+`;
+
+const UL = styled('ul')`
+	display: flex;
+	flex-direction: column;
+	justify-content: start;
+	gap: 1.5rem;
+	color: ${theme.text.white.primary};
+	font-size: 1.2rem;
+	li {
+		cursor: pointer;
+	}
+`;
 
 export default LeftSideBar;
