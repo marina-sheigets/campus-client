3aefc9390f0b2222e70b53c1e778cf29d17f12c6
Marina Sheigets <marinasejgec34@gmail.com>
2023-01-29 21:04:36 +0200

    feat: add navigation

--- src/components/__molecules__/LeftSideBar/LeftSideBar.tsx
+++ src/components/__molecules__/LeftSideBar/LeftSideBar.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import theme from '../../../constants/globalStyles';
 
 const LINKS = [
 	{
 		id: 1,
-		name: 'Create Article',
+		name: 'Add Article',
 	},
 	{
 		id: 2,
@@ -28,12 +29,68 @@ const LINKS = [
 		name: 'Add Subject',
 	},
 ];
+
 function LeftSideBar() {
+	const [path, setPath] = useState('');
+	const [currentPath, setCurrentPath] = useState<number>(0);
+	const navigate = useNavigate();
+	const handleSetPath = (value: string) => setPath(value);
+
+	useEffect(() => {
+		switch (path) {
+			case LINKS[0].name:
+				navigate('/new/article');
+				break;
+			case LINKS[1].name:
+				navigate('/new/student');
+				break;
+			case LINKS[2].name:
+				navigate('/new/teacher');
+				break;
+			case LINKS[3].name:
+				navigate('/new/group');
+				break;
+			case LINKS[4].name:
+				navigate('/new/faculty');
+				break;
+			case LINKS[5].name:
+				navigate('/new/subject');
+				break;
+		}
+	}, [path, navigate]);
+
+	useEffect(() => {
+		const url = window.location.pathname;
+		let location = 0;
+		switch (url) {
+			case '/new/article':
+				location = LINKS[0].id;
+				break;
+			case '/new/student':
+				location = LINKS[1].id;
+				break;
+			case '/new/teacher':
+				location = LINKS[2].id;
+				break;
+			case '/new/group':
+				location = LINKS[3].id;
+				break;
+			case '/new/faculty':
+				location = LINKS[4].id;
+				break;
+			case '/new/subject':
+				location = LINKS[5].id;
+				break;
+		}
+		setCurrentPath(location);
+	}, []);
 	return (
 		<Wrapper>
-			<UL>
+			<UL currentPath={currentPath}>
 				{LINKS.map((item) => (
-					<li key={item.id}>{item.name}</li>
+					<li key={item.id} onClick={() => handleSetPath(item.name)}>
+						{item.name}
+					</li>
 				))}
 			</UL>
 		</Wrapper>
@@ -48,7 +105,7 @@ const Wrapper = styled('div')`
 	z-index: 1;
 `;
 
-const UL = styled('ul')`
+const UL = styled('ul')<{ currentPath: number }>`
 	display: flex;
 	flex-direction: column;
 	justify-content: start;
@@ -57,6 +114,10 @@ const UL = styled('ul')`
 	font-size: 1.2rem;
 	li {
 		cursor: pointer;
+	}
+
+	li: nth-child(${(props) => props.currentPath}) {
+		color: ${theme.text.white.light};
 	}
 `;
 
