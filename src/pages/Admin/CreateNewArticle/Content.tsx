29e090d4a745f09ef7b73a2fde04f0d2db8560a6
Marina Sheigets <marinasejgec34@gmail.com>
2023-01-29 21:06:24 +0200

    feat: add logic for createArticle page

--- src/pages/Admin/CreateNewArticle/Content.tsx
+++ src/pages/Admin/CreateNewArticle/Content.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import styled from 'styled-components';
 import NewspaperIcon from '@mui/icons-material/Newspaper';
 import { TextField, Button } from '@mui/material';
@@ -6,12 +6,59 @@ import LabelBox from '../../../components/__features__/LabelBox/LabelBox';
 import Forms from '../../../components/__atoms__/Forms/Forms';
 import Form from '../../../components/__atoms__/Form/Form';
 import theme from '../../../constants/globalStyles';
-import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import CreatePageWrapper from '../../../components/__atoms__/CreatePageWrapper/CreatePageWrapper';
 import TitleBox from '../../../components/__atoms__/TitleBox/TitleBox';
 import CreatePageTitle from '../../../components/__atoms__/CreatePageTitle/CreatePageTitle';
 
 function Content() {
+	const [articleName, setArticleName] = useState('');
+	const [articleContent, setArticleContent] = useState('');
+	const [links, setLinks] = useState([{ id: 1, url: '' }]);
+
+	const nameMaxChar = 100;
+	const contentMaxChar = 2000;
+
+	const handleChangeArticleName = (e: any) => {
+		if (e.target.value.length > nameMaxChar) {
+			return;
+		}
+		setArticleName(e.target.value);
+	};
+	const handleChangeArticleContent = (e: any) => {
+		if (e.target.value.length > contentMaxChar) {
+			return;
+		}
+		setArticleContent(e.target.value);
+	};
+
+	const handleAddLink = () => {
+		setLinks([...links, { id: Date.now(), url: '' }]);
+	};
+
+	const handleDeleteLink = (id: number) => {
+		setLinks(links.filter((link) => link.id !== id));
+	};
+
+	const handleChangeLink = (url: string, id: number) => {
+		const updatedLinks = links.map((link) => {
+			if (link.id === id) {
+				return {
+					...link,
+					url,
+				};
+			}
+			return link;
+		});
+		setLinks(updatedLinks);
+	};
+
+	const currentNameLength = useMemo(() => articleName.length, [articleName]);
+	const currentContentLength = useMemo(() => articleContent.length, [articleContent]);
+	const isAllCompleted = useMemo(() => {
+		return articleName.trim().length && articleContent.trim().length;
+	}, [articleName, articleContent]);
+
 	return (
 		<CreatePageWrapper>
 			<TitleBox>
@@ -23,34 +70,48 @@ function Content() {
 
 			<Forms>
 				<StyledForm>
-					<LabelBox maxOfCharacters={100} label={'Name of article'} currentAmount={0} />
-					<TextField size='small' placeholder='Enter the title of article' />
+					<LabelBox
+						maxOfCharacters={nameMaxChar}
+						label={'Name of article'}
+						currentAmount={currentNameLength}
+					/>
+					<TextField
+						size='small'
+						placeholder='Enter the title of article'
+						value={articleName}
+						onChange={handleChangeArticleName}
+					/>
 				</StyledForm>
 				<Form>
 					<LabelBox
-						maxOfCharacters={2000}
+						maxOfCharacters={contentMaxChar}
 						label={'Enter the content of article'}
-						currentAmount={0}
+						currentAmount={currentContentLength}
 					/>
-					<Textarea />
+					<Textarea value={articleContent} onChange={handleChangeArticleContent} />
 				</Form>
 				<LinkForm>
 					<LabelBox label={'You can add one link or click on button to add more'} />
-					<LinkBox>
-						<LinkField size='small' placeholder='New link' />
-						<AddCircleOutlineOutlinedIcon fontSize='large' />
-					</LinkBox>
-					<LinkBox>
-						<LinkField size='small' placeholder='New link' />
-						<AddCircleOutlineOutlinedIcon fontSize='large' />
-					</LinkBox>
-					<LinkBox>
-						<LinkField size='small' placeholder='New link' />
-						<AddCircleOutlineOutlinedIcon fontSize='large' />
-					</LinkBox>
+					<AddLinkButton variant='outlined' onClick={handleAddLink}>
+						Add Link
+					</AddLinkButton>
+
+					{links.map((link) => (
+						<LinkBox key={link.id}>
+							<LinkField
+								size='small'
+								placeholder='New link'
+								value={link.url}
+								onChange={(e: any) => handleChangeLink(e.target.value, link.id)}
+							/>
+							<DeleteOutlinedIcon onClick={() => handleDeleteLink(link.id)} />
+						</LinkBox>
+					))}
 				</LinkForm>
 			</Forms>
-			<FinishButton variant='contained'>Finish</FinishButton>
+			<FinishButton disabled={!isAllCompleted} variant='contained'>
+				Finish
+			</FinishButton>
 		</CreatePageWrapper>
 	);
 }
@@ -61,6 +122,9 @@ const FinishButton = styled(Button)`
 	margin-bottom: 1rem;
 `;
 
+const AddLinkButton = styled(Button)`
+	width: 300px;
+`;
 const LinkField = styled(TextField)`
 	flex: 8;
 `;
@@ -98,6 +162,8 @@ const Textarea = styled('textarea')`
 	outline: none;
 	height: 250px;
 	border-radius: 10px;
+	font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+	font-size: 1rem;
 `;
 
 export default Content;
